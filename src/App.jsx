8ab9487015b68a91bc739c1bce904442c4bd861b
sarbajit <sarbajit>
2025-08-21 14:08:42 +0530

    version 7 updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import Home from "./pages/Home";
 import CreateAccount from "./pages/CreateAccount";
-import AccountDetails from "./pages/AccountDetails ";
+import AccountDetails from "./pages/AccountDetails";
 import Login from "./pages/Login";
 
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/create-account", element: <CreateAccount /> },
+  { path: "/account", element: <AccountDetails /> },
+  { path: "/login", element: <Login /> },
+]);
+
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-account" element={<CreateAccount />} />
-        <Route path="/account" element={<AccountDetails />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
